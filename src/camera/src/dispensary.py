c86d0b929a592e26eabbca1d0b1a12c08fbf1f16
FelipeFerreira13 <felipeferreira1310@hotmail.com>
2024-08-26 02:04:41 -1000

    choose obj closer to the center

--- src/camera/src/dispensary.py
+++ src/camera/src/dispensary.py
@@ -7,22 +7,22 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 
-desired_x = 175
-desired_y = 175
+desired_x = 180
+desired_y = 59
 
 lower_blue   = ( 100, 100,  50 )
 upper_blue   = ( 130, 255, 255 )
-area_blue    = 3400
+area_blue    = 3900
 
-lower_red    = ( 170,  25,  25 )
+lower_red    = ( 170,  50,  50 )
 upper_red    = ( 180, 220, 220 )
-lower_red_h  = ( 0,  25,  25 )
+lower_red_h  = ( 0,  50,  50 )
 upper_red_h  = ( 10, 220, 220 )
 area_red     = 2100
 
 lower_yellow = ( 20,  100, 100 )
 upper_yellow = ( 35,  255, 255 )
-area_yellow  = 3450
+area_yellow  = 4100
 
 class disp():
     def __init__( self ):
@@ -50,11 +50,14 @@ class disp():
             # Capture frame-by-frame
             ret, frame = cap.read()
             frame = cv2.resize( frame, None, fx = 0.5, fy = 0.5 )
+
+            shape = frame.shape
                 
             if ret == True:
 
                 cube = req.cube
                 area_cube = 0
+
 
                 hsv = cv2.cvtColor( frame, cv2.COLOR_BGR2HSV )
 
@@ -66,6 +69,7 @@ class disp():
                 elif ( cube == 'white' ):
                     mask = cv2.inRange( hsv, lower_blue, upper_blue )
                     kernel = np.ones((6,6))
+                    mask_b = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations = 2)
                     mask_b = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations = 5)
                     cv2.imshow('blue_close', mask_b)
 
@@ -88,48 +92,57 @@ class disp():
                 
                 kernel = np.ones((3,3))
                 mask = cv2.morphologyEx( mask, cv2.MORPH_OPEN, kernel,  iterations = 1)
+                mask = cv2.morphologyEx( mask, cv2.MORPH_CLOSE, kernel,  iterations = 1)
+
 
                 c, h = cv2.findContours( mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
 
-                max_area = 0
+                x_center = 1000
+                c_info = []
                 x_ = 0
                 y_ = 0
+                area = 0
                 for cnts in c:
                     ( x, y, w, h ) = cv2.boundingRect(cnts)
 
-                    area = w * h
+                    delta_x = abs((shape[1] / 2) - (x + ( w / 2 )))
+                    
 
-
-                    if area > max_area:
-                        max_area = area
+                    if delta_x < x_center and ( w * h ) > 300:
+                        area = ( w * h )
+                        x_center = delta_x
                         x_ = x + ( w / 2 )
                         y_ = y + ( y / 2 )
+                        c_info = [ x, y, w, h ]
 
-                print("x: ", x_, "y: ", y_, "max_area: ", max_area)
+                print("x: ", x_, "y: ", y_, "x_center: ", x_center, "area: ", c_info[2] * c_info[3])
+
+                cv2.rectangle( frame, (c_info[0], c_info[1]), (c_info[0] + c_info[2], c_info[1] + c_info[3]), (0,0,255), 1 )
 
                 vx  = 0
                 vy  = 0
                 vth = 0
                 vz  = 0
 
-                max_linear_speed   = 0.1
+                max_linear_speed   = 0.15
                 max_elevator_speed = 0.075
                 max_angular_speed  = 0.4
 
-                if   ( abs( x_ - desired_x ) > 15 ):
-                    speed = (abs( x_ - desired_x ) / 50.0) * max_linear_speed
+                if   ( abs( x_ - desired_x ) > 10 ):
+                    speed = (abs( x_ - desired_x ) / 75.0) * max_linear_speed
                     if ( speed > max_linear_speed): speed = max_linear_speed
                     vy = (( x_ - desired_x ) / abs( x_ - desired_x )) * speed
 
-                elif ( abs( y_ - desired_y ) > 15 ):
-                    speed = (abs( y_ - desired_y ) / 50.0) * max_elevator_speed
+                elif ( abs( y_ - desired_y ) > 10 ):
+                    speed = (abs( y_ - desired_y ) / 35.0) * max_elevator_speed
                     if ( speed > max_elevator_speed): speed = max_elevator_speed
                     vz = (( y_ - desired_y ) / abs( y_ - desired_y )) * speed
 
-                elif ( abs( max_area - area_cube ) > 150 ):
-                    speed = (abs( max_area - area_cube ) / 1000.0) * 0.1
+                elif ( abs( area - area_cube ) > 150 ):
+                    speed = (abs( area - area_cube ) / 1500.0) * 0.1
                     if ( speed > max_linear_speed): speed = max_linear_speed
-                    vx = (( area_cube - max_area ) / abs( area_cube - max_area )) * speed
+                    vx = (( area_cube - area ) / abs( area_cube - area )) * speed
+
 
                 th_diff = req.angle - self.th
                 if   ( th_diff > 180 ):
@@ -137,7 +150,7 @@ class disp():
                 elif ( th_diff < -180 ):
                     th_diff = th_diff + 360
 
-                if( abs( th_diff ) > 3 ):
+                if( abs( th_diff ) > 0.5 ):
                     vth = (th_diff / 10.0) * max_angular_speed
                     if   ( vth > max_angular_speed ):
                         vth = max_angular_speed
@@ -159,14 +172,14 @@ class disp():
                 if count > 3:
                     break
 
-                cv2.imshow('image', frame)
-                cv2.imshow('mask', mask)
+                # cv2.imshow('image', frame)
+                # cv2.imshow('mask', mask)
                     
                 cv2.waitKey(1)
                     
             # Sleep just enough to maintain the desired rate
             rate.sleep()
-
+        cv2.destroyAllWindows()
         return True
 
 
